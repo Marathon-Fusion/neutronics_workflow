de9e8ce7935ff1974194f76c295fae6d239f20fd
VictorLowingsCam <vl330@cam.ac.uk>
2025-07-24 16:02:23 -0700

    Removed old/irrelevant stuff

--- Code/workspace/src/My_crap/.tar.gz unpacker.py
+++
@@ -1,7 +0,0 @@
-import tarfile
-
-file = tarfile.open(r'Downloads\jendl-f99.tar.gz')
-
-print(file.getnames())
-file.extractall(r'C:\Users\lowin\Documents\Marathon\nuclear-parser-temp\nuclear-parser-temp\My crap\JENDL fusion data')
-file.close()
